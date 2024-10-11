494a228850589c6580e494e94793827fd43aff48
zwj <zwj@dd-robot.net>
2024-10-11 23:49:59 +0800

    配置完开发环境

---
+++ helloworld.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#include "max.h"
+
+int main()
+{
+    for (int i = 0; i < 5; i++)
+        printf("Hello Grayson~%d\n", i); 
+
+    int result = findMaxNum(1,3);
+
+    printf("result=%d",result);
+
+    int a = 10;
+    printf("%d",a);
+
+    return 0;
+}
---
+++ max.h
@@ -0,0 +1,10 @@
+#ifndef __MAX_H__
+#define __MAX_H__
+#include <stdio.h>
+
+extern "C"
+{
+    int findMaxNum(int num1, int num2);
+}
+
+#endif // __MAX_H__
